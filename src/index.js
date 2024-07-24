2c966a5b053e1f5be8d12a33a54ddeb39466fbf8
James Olinya <olinyajames@gmail.com>
2024-07-24 19:44:53 +0100

    use parcel to start sdk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import YouverifyLiveness from 'youverify-liveness-web';
+import YouverifyLiveness from "youverify-liveness-web";
+const yvLiveness = new YouverifyLiveness({
+  tasks: [{ id: 'complete-the-circle' }],
+});
 
 document.getElementById('start-liveness').addEventListener('click', () => {
   try {
-    const yvLiveness = new YouverifyLiveness({
-      tasks: [{ id: 'complete-the-circle' }],
-    });
     yvLiveness.start();
   } catch (error) {
     // Handle Validation Errors
